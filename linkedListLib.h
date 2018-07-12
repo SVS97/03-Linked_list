ff09fac7976b50230d2d09f8df27d2cd119d754d
SVS97 <SVS97@i.ua>
2018-07-12 19:37:59 +0300

    First commit

--- linkedListLib.h
+++ linkedListLib.h
@@ -18,7 +18,7 @@ void linked_list_tail_add		(Node *list_head, int value);
 void linked_list_insert			(Node *list_head, unsigned n, int val);
 int  linked_list_remove			(Node **list_head, int n);
 void linked_list_clear			(Node **list_head);
-int linked_list_count_identical     (Node *list_head, int n);
+int linked_list_count_identical         (Node *list_head, int n);
 void linked_list_reverse		(Node** list_head);
 void linked_list_sort			(Node * list_head);
 
